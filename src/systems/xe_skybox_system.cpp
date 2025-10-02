c0ab447c0103d57b458209325fde9c46de656406
tigeradi99 <adityanair483@gmail.com>
2025-10-02 18:57:21 +0530

    Add skybox skeleton

---
+++ src/systems/xe_skybox_system.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adity on 02-10-2025.
+//
+
+#include "systems/xe_skybox_system.h"
---
+++ src/systems/xe_skybox_system.h
@@ -0,0 +1,19 @@
+//
+// Created by adity on 02-10-2025.
+//
+
+#pragma once
+
+#include "vulkan/vulkan.h"
+#include "renderer/xe_pipeline.h"
+#include "renderer/xe_image_vma.h"
+#include "systems/xe_frame_info.h"
+#include "renderer/xe_descriptors.h"
+#include "renderer/xe_buffer.h"
+
+#include <memory>
+#include <array>
+
+namespace xe {
+
+}
